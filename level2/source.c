59e28404a94cedb117325e69fb6a7a77b90c49ba
manaccac <maximenaccache@gmail.com>
2021-11-25 17:35:53 +0100

    level2 OK

--- level2/source.c
+++ level2/source.c
@@ -3,22 +3,21 @@
 void p()
 {
 	unsigned int i;
-	char str[100];//sub    $0x68,%esp
+	char str[80];
 
-	fflush();//call   0x80483b0 <fflush@plt>
+	fflush(stdout);//call   0x80483b0 <fflush@plt>
 	gets(str);//call   0x80483c0 <gets@plt>
 
 	if (0xb0000000 && i) // 0x080484fb <+39>:    and    $0xb0000000,%eax
 	{
 		if (0xb0000000 == i) //   0x08048500 <+44>:    cmp    $0xb0000000,%eax
 		{
-			printf(); //call   0x80483a0 <printf@plt>
+			printf("%p\n", i); //call   0x80483a0 <printf@plt>
 			exit(); //call   0x80483d0 <_exit@plt>
 		}
 	}
-	pust();
-	strdup();
-	return ;
+	pust(str); //call   0x80483f0 <puts@plt>
+	return (strdup(str)); // call   0x80483e0 <strdup@plt>
 }
 
 int main(){
