93e15267d4e0dad180e3f4cef1a0c76c4be1d0f8
manaccac <maximenaccache@gmail.com>
2021-11-25 11:51:31 +0100

    level1 OK

---
+++ level1/source.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int run(void) {
+    fwrite("good machin truc", 1, 16, stdout)
+    system("/bin/sh");
+}
+
+void main() {
+    char str[76];
+
+    gets(str);
+    return;
+}
